5d54432cfb9cd8d879cc6ed8eea54b4190d19d2c
barld <bakeds@gmail.com>
2016-03-09 15:25:38 +0100

    Initial base comment:     working monogame project     display green background with road

---
+++ MonoSimulatieFSharp/Program.fs
@@ -0,0 +1,8 @@
+﻿open game
+
+[<EntryPoint>]
+let main argv =     
+    use g = new SimulationGame()
+    g.Run()
+
+    0 // return an integer exit code
---
+++ MonoSimulatieFSharp/SimulationGame.fs
@@ -0,0 +1,35 @@
+﻿
+module game
+
+open Microsoft.Xna.Framework
+open Microsoft.Xna.Framework.Graphics
+open Microsoft.Xna.Framework.Input
+
+type SimulationGame () as this =
+    inherit Game()
+
+    do this.Content.RootDirectory <- "Content"
+    let graphics = new GraphicsDeviceManager(this)
+    let mutable spriteBatch = Unchecked.defaultof<SpriteBatch>
+    let mutable texture = Unchecked.defaultof<Texture2D>
+
+    override this.Initialize() =
+        do base.Initialize()
+
+    override this.LoadContent() =
+        do spriteBatch <- new SpriteBatch(this.GraphicsDevice)
+        ()
+
+    override this.Update (gameTime) =
+        ()
+
+    override this.Draw (gameTime) =
+        do this.GraphicsDevice.Clear Color.LightGreen       
+        spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend);
+
+        let background = this.Content.Load<Texture2D>("background.png")
+
+        spriteBatch.Draw(background, Vector2.Zero, Color.White);
+
+        spriteBatch.End() 
+        ()
